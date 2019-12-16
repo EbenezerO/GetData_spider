9de16abaad8984ab54989a0f7c12a8da14d06e1e
Ouyang <1305805709@qq.com>
2019-12-16 15:56:24 +0800

    score info 2.0

--- t2_score.py
+++ t2_score.py
@@ -14,8 +14,7 @@ from itertools import chain
 参数：pageSource某页的源码
 返回值：返回爬取课程中一页的所有用户的allUserName,allUserUrl,allContent,allScore
 '''
-def getPageScore(data):
-    soup = BeautifulSoup(data, 'html.parser')
+def getPageScore(soup):
     user_info=soup.find_all('div',{'class':'ux-mooc-comment-course-comment_comment-list_item_body_user-info'}) #所有用户信息
     content = soup.find_all('div',{'class': 'ux-mooc-comment-course-comment_comment-list_item_body_content'})  # 包含全部评论项目的总表标签
     allUserName=[]
@@ -31,7 +30,7 @@ def getPageScore(data):
 
         #用户评分
         sScore=user_info[i].find_all('i',{'class':'star ux-icon-custom-rating-favorite'})
-        allScore.append(len(sScore))
+        allScore.append(str(len(sScore)))
 
         #评论内容
         sContent = content[i].find_all('span')
@@ -49,7 +48,7 @@ def get_score_data(url):
     chrome = webdriver.PhantomJS(executable_path=r"D:\phantomjs-2.1.1-windows\bin\phantomjs.exe")
     chrome.get(url)
     chrome.find_element_by_id("review-tag-button").click()
-    time.sleep(2)
+    time.sleep(1)
     count = 1
 
     allUserName = []
@@ -59,32 +58,33 @@ def get_score_data(url):
 
     errorUrl=[]
     while(True):
-        try:
-            data = chrome.page_source
-            tempUserName, tempUserUrl, tempContent, tempScore = getPageScore(data)
-            for i in range(len(tempScore)):
-                allUserName.append(tempUserName[i])
-                allUserUrl.append(tempUserUrl[i])
-                allContent.append(tempContent[i])
-                allScore.append(tempScore[i])
-
-            #print(count)
-            soup = BeautifulSoup(data, 'html.parser')
-            next = soup.find_all('li', {'class': 'ux-pager_btn ux-pager_btn__next'})
-            if (next[0].contents[1].attrs['class'] == ['th-bk-disable-gh']  ):
-                #print("到最后一页,成功结束")
-                break
-            chrome.find_element_by_link_text("下一页").click()
-            time.sleep(2)
-            count += 1
-        except:
+        data = chrome.page_source
+        soup = BeautifulSoup(data, 'html.parser')
+        no_comment = soup.find_all('div', {'class': 'ux-mooc-comment-course-comment_no-comment'})
+        if (len(no_comment) == 1): # 没有评论
             errorUrl.append(url)
             break
+        tempUserName, tempUserUrl, tempContent, tempScore = getPageScore(soup)
+        for i in range(len(tempScore)):
+            allUserName.append(tempUserName[i])
+            allUserUrl.append(tempUserUrl[i])
+            allContent.append(tempContent[i])
+            allScore.append(tempScore[i])
+
+        # print(count)
+        next = soup.find_all('li', {'class': 'ux-pager_btn ux-pager_btn__next'})
+        if (len(next) == 0 or next[0].contents[1].attrs['class'] == ['th-bk-disable-gh']):  # 不满一页 或者 完成爬取
+            # print("到最后一页,成功结束")
+            break
+        chrome.find_element_by_link_text("下一页").click()
+        time.sleep(2)
+        count += 1
 
     chrome.quit()
     return allUserName, allUserUrl, allContent, allScore,errorUrl
 
-#get_score_data('http://www.icourse163.org/course/ECJTU-1206862806')
+
+#get_score_data('http://www.icourse163.org/course/ZJU-1206061801')
 
 if __name__=='__main__':
     file = pd.read_csv('pandas_allurl.csv', usecols=['id','url'])
@@ -94,19 +94,18 @@ if __name__=='__main__':
         writer = csv.DictWriter(csvfile, fieldnames=field)
         writer.writeheader()
 
-        with open('error.csv', 'w', encoding='utf_8_sig') as csvfile1:
-            field1 = ['error_id', 'error_url']
-            writer1 = csv.DictWriter(csvfile1, fieldnames=field1)
-            writer1.writeheader()
-
         for i in range(len(df)):
             document = df[i:i + 1]
             course_url = document['url'][i]
-            course_id =document['id'][i]
+            course_id = document['id'][i]
             print(course_id)
-            allUserName, allUserUrl, allContent, allScore,errorUrl = get_score_data(course_url)
-            if errorUrl: #课程没有评论 或者  不满一页
-                writer1.writerow({'error_id':course_id,'error_url':errorUrl})
+            allUserName, allUserUrl, allContent, allScore, errorUrl = get_score_data(course_url)
+            if errorUrl:  # 课程没有评论
+                print(str(course_id)+','+errorUrl[0])
             for j in range(len(allScore)):
-                writer.writerow({'course_id': course_id, 'course_url': course_url, 'user_name':allUserName[j], 'user_url':allUserUrl[j], 'score':allScore[j],'content':allContent[j]})
+                #print(str(course_id)+','+course_url+','+allUserName[j]+','+allUserUrl[j]+','+allScore[j]+','+allContent[j])
+                writer.writerow({'course_id': str(course_id), 'course_url': course_url, 'user_name': allUserName[j],'user_url': allUserUrl[j], 'score': allScore[j], 'content': allContent[j]})
+            time.sleep(2)
+
+
 
